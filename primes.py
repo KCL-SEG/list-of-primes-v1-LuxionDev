ac11927b148d48b2672d29acc0c389f2d8ee3889
George Korodimos <61911511+LuxionDev@users.noreply.github.com>
2022-10-18 13:49:53 +0000

    completed excercise

--- primes.py
+++ primes.py
@@ -1,6 +1,20 @@
-"""List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
+#List of prime numbers generator.
+#ENTER YOUR SOLUTION HERE!
 
 def primes(number_of_primes):
-    list = []
+    list = [2]
+    i =3
+    while len(list) < number_of_primes:
+        prime = True
+        for j in list:
+            if i % j == 0:
+                prime = False
+                break
+        if prime:
+            #print(i)
+            list.append(i)
+    print(list)
     return list
+
+
+primes (2)
